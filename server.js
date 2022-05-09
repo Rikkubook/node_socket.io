d101bf1de73f3916d2fbde705151a47d648d09b1
Rikku <rikkubook@gmail.com>
2022-05-09 22:21:11 +0800

    調整格式

--- server.js
+++ server.js
@@ -11,9 +11,17 @@ const http = require('http');
 const server = http.createServer(app);
 
 
+
 const { Server } = require("socket.io");
 const io = new Server(server);
-let roomNo ='';
+
+let online =[
+  { name: 'Rikku', roomNo : 2},
+  { name: 'LuLu', roomNo : 1},
+  { name: 'Keven', roomNo : 1},
+]
+
+let roomNo = []; // 2
 let onlineCount = 0;
 const users = {}
 //用 api 方式建立連線
@@ -23,19 +31,19 @@ app.get('/', function (req, res) {
 })
 
 
-io.on('connection', function (socket) {
-  socket.join(roomNo); //進入
+io.on('connection', function (socket) { //1
+  // socket.join(roomNo); //創立
   onlineCount = io.sockets.adapter.rooms.get(roomNo).size //目前上線人數
 
-  socket.on('join', (name) => { 
-    console.log(socket.id)
-    users[socket.id] = name; //給唯一key
-    io.in(roomNo).emit('join the room', name); 
+  socket.on('join', (name) => { //3
+    console.log(socket.id) //sdfsdfsdf
+    users[socket.id] = name; //給唯一key  /sdsfsdfd=Rikku
+    io.in(roomNo).emit('join the room', name);  // 4
   });
 
   socket.on('chat message', (msg) => { // 當觸發 'chat message' 拿到送出的訊息
     console.log(msg)
-    io.in(roomNo).emit('get chat message', msg); //消息發送給所有人，包括發件人。
+    io.in(roomNo).emit('get chat message', msg); //消息發送給所有人，包括發件人。 //2
   });
 
   socket.on('disconnect', (msg) => {
